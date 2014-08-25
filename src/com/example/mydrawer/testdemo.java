eeb548e35fca1e24447526a1fc84eb67f10adb5c
feiyuhuapeng <feiyuhuapeng@163.com>
2014-08-25 11:04:44 +0800

    add testdemo

---
+++ src/com/example/mydrawer/testdemo.java
@@ -0,0 +1,15 @@
+package com.example.mydrawer;
+
+import android.app.Activity;
+
+public class testdemo extends Activity {
+    private int a=0;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
